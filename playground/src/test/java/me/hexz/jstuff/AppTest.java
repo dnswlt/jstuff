369ea3236baf486099648c7540c6cdc42e425bd8
Dennis Walter <dennis.walter@gmail.com>
2025-06-15 17:35:53 +0200

    Modern Java Hello World.

---
+++ playground/src/test/java/me/hexz/jstuff/AppTest.java
@@ -0,0 +1,11 @@
+package me.hexz.jstuff;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test
+    void sampleTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
